1cdcbc21f5bcef0679c1b67214c455dd4740e6ba
shimahi <shimahi@rear.jp>
2020-12-27 22:15:41 +0900

    firebase設定

--- src/components/_layouts/Layout.tsx
+++ src/components/_layouts/Layout.tsx
@@ -9,7 +9,7 @@ export const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <Head>
-        <title>Document</title>
+        <title>TODO APP</title>
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <main>{children}</main>
