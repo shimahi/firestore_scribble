0effb78c150ecb60e4c915963530a0f1016714f5
shimahi <shimahi@rear.jp>
2020-12-27 23:39:31 +0900

    add new tast

--- src/components/templates/IndexTemplate.tsx
+++ src/components/templates/IndexTemplate.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
-
+import { FormControl, TextField } from '@material-ui/core'
 import { db } from '_firebase'
+import AddToPhotosIcon from '@material-ui/icons/AddToPhotos'
 
 export const IndexTemplate = () => {
   const [tasks, setTasks] = useState([
@@ -9,6 +10,8 @@ export const IndexTemplate = () => {
       title: '',
     },
   ])
+
+  const [input, setInput] = useState('')
 
   useEffect(() => {
     const unSub = db.collection('tasks').onSnapshot((snapshot) => {
@@ -23,15 +26,41 @@ export const IndexTemplate = () => {
     return () => unSub()
   }, [])
 
+  const newTask = (/* e: React.MouseEvent<HTMLButtonElement> */) => {
+    db.collection('tasks').add({
+      title: input,
+    })
+    setInput('')
+  }
+
   return (
-    <div>
-      {tasks.map((task) => {
+    <section>
+      <h1>TODO APP</h1>
+
+      <FormControl>
+        <TextField
+          label="New task ?"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          value={input}
+          onChange={({ target: { value } }) => {
+            setInput(value)
+          }}
+        />
+      </FormControl>
+      <button disabled={!input} onClick={newTask}>
+        <AddToPhotosIcon />
+      </button>
+      <hr />
+
+      {tasks.map(({ id, title }) => {
         return (
-          <div key={task.id}>
-            <h2>{task.title}</h2>
+          <div key={id}>
+            <h2>{title}</h2>
           </div>
         )
       })}
-    </div>
+    </section>
   )
 }
